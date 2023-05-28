3796918caaa11088978c159911f659902478b6ff
shimiwaka <kei_ikeda@r.recruit.co.jp>
2023-05-28 23:33:33 +0900

    wip

--- okodukai-react/src/pages/Board.tsx
+++ okodukai-react/src/pages/Board.tsx
@@ -1,5 +1,15 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
 
-const Board = () => {
+const targetURL: string = process.env.REACT_APP_BASE_URL || "";
+
+const Board = () => {  
+  const params = useParams();
+
+  React.useEffect(() => {
+    alert(params.token);
+  }, [params.token]);
+
   return (
     <>
       <div>
--- okodukai-react/src/pages/Top.tsx
+++ okodukai-react/src/pages/Top.tsx
@@ -1,14 +1,20 @@
 import axios from "axios";
+import React from 'react';
+import { useNavigate } from "react-router-dom"
 
 const targetURL: string = process.env.REACT_APP_BASE_URL || "";
 
 const Top = () => {
+  const [email, setEmail] = React.useState<string>("");
+
+  const navigate = useNavigate()
   const createBoard = () => {
-    axios.post(targetURL + "create", { email: "hoge@email.com" }, {
+    axios.post(targetURL + "create", { email: email }, {
       headers: {'Content-Type': 'application/x-www-form-urlencoded'}
     })
-    .then((response) => {
-      console.log(response);
+    .then((response : any) => {
+      console.log(response.data);
+      navigate("/board/" + response.data.token)
     })
   }
 
@@ -17,7 +23,7 @@ const Top = () => {
       <div>
         おこづかいシートを作るには、メールアドレスを入力してください。
       </div>
-      <input placeholder="メールアドレス"/>
+      <input placeholder="メールアドレス" onChange={(e) => setEmail(e.target.value)}/>
       <button type="submit" onClick={createBoard}>
         OK
       </button>
