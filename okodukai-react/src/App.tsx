33160f83a8585e7f5da9d85d8d039492605a8e95
shimiwaka <kei_ikeda@r.recruit.co.jp>
2023-05-28 21:54:23 +0900

    create base parts

--- okodukai-react/src/App.tsx
+++ okodukai-react/src/App.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Top from './pages/Top'
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <header>
+      <span className="Title">
+        <Link to="/">
+          <h3>おこづかいシート</h3>
+        </Link>
+      </span>
+    </header>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Top />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
---
+++ okodukai-react/src/pages/Top.tsx
@@ -0,0 +1,16 @@
+
+const Top = () => {
+  return (
+    <>
+      <div>
+        おこづかいシートを作るには、メールアドレスを入力してください。
+      </div>
+      <input placeholder="メールアドレス"/>
+      <button>
+        OK
+      </button>
+    </>
+  )
+}
+
+export default Top;
