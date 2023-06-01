8e0c9656b9ffe9d7e393fc72f8431d496dfe0995
shimiwaka <kei_ikeda@r.recruit.co.jp>
2023-06-01 22:07:14 +0900

    add price to column

--- okodukai-react/src/pages/Board.tsx
+++ okodukai-react/src/pages/Board.tsx
@@ -8,6 +8,7 @@ const Board = () => {
   const params = useParams();
   const [owner, setOwner] = React.useState<string>();
   const [newColumn, setNewColumn] = React.useState<string>("");
+  const [newPrice, setNewPrice] = React.useState<number>(100);
   const [columns, setColumns] = React.useState<any[]>([]);
   const [table, setTable] = React.useState<any[]>([]);
 
@@ -32,6 +33,7 @@ const Board = () => {
     }
     var postParams = new URLSearchParams()
     postParams.append('name', newColumn)
+    postParams.append('price', String(newPrice))
     axios.post(targetURL + "board/" + params.token + "/newcolumn" , postParams)
     .then((response : any) => {
       setNewColumn("")
@@ -56,6 +58,7 @@ const Board = () => {
       </div>
       <div>
         <input placeholder="おてつだいを追加" value={newColumn} onChange={(e) => setNewColumn(e.target.value)}/>
+        <input placeholder="単価" value={newPrice} onChange={(e) => setNewPrice(Number(e.target.value))}/>円
         <button type="submit" onClick={createColumn}>
           追加
         </button>
@@ -66,7 +69,7 @@ const Board = () => {
             <td>
               日付
             </td>
-            { columns.map((value) => <td>{value.name}</td>)}
+            { columns.map((value) => <td>{value.name}<br/>{value.price}</td>)}
           </tr>
           {
             table.map((value) => { 
